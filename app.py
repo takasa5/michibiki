bf7937340a820a18964656dac2f8a725a0ae5a2a
takasa5 <vipqiv00@gmail.com>
2018-02-02 22:01:20 +0900

    without cv2

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-import cv2
+#import cv2
 import numpy as np
 import base64
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from PIL import Image
 from io import BytesIO
-import Constellation
-import stardust
+#import Constellation
+#import stardust
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kingofsecretyeahhhhh'
@@ -30,8 +30,8 @@ def test_message(message):
 def test_image(message):
     print("recv")
     #emit('my_response', {'data': message['data']})
-    img = readb64(message['data'].split("data:image/jpeg;base64,")[1])
-    stardust.trace(img, Constellation.Sagittarius())
+    #img = readb64(message['data'].split("data:image/jpeg;base64,")[1])
+    #stardust.trace(img, Constellation.Sagittarius())
     print("saved")
     emit('my_response', {'data': "traced"})
 
@@ -40,8 +40,8 @@ def readb64(b64_str):
     sbuf.write(base64.b64decode(b64_str))
     pimg = Image.open(sbuf)
     cv2img = np.asarray(pimg)
-    #RGB->BGR変換
-    return cv2img[:,:,::-1].copy()
+    #RGB->BGR
+    #return cv2img[:,:,::-1].copy()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
