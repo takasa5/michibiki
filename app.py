eedd2acd9baef511dbdd1b5d4efb0b61b16dfc23
takasa5 <vipqiv00@gmail.com>
2018-02-03 22:27:30 +0900

    add buildpack

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-#import cv2
+import cv2
 import numpy as np
 import base64
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from PIL import Image
 from io import BytesIO
-#import Constellation
-#import stardust
+import Constellation
+import stardust
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'kingofsecretyeahhhhh'
+#app.config['SECRET_KEY'] = 'kingofsecretyeahhhhh'
 socketio = SocketIO(app)
 
 @app.route('/')
@@ -29,9 +29,9 @@ def test_message(message):
 @socketio.on('my_image', namespace='/test')
 def test_image(message):
     print("recv")
-    #emit('my_response', {'data': message['data']})
-    #img = readb64(message['data'].split("data:image/jpeg;base64,")[1])
-    #stardust.trace(img, Constellation.Sagittarius())
+    emit('my_response', {'data': message['data']})
+    img = readb64(message['data'].split("data:image/jpeg;base64,")[1])
+    stardust.trace(img, Constellation.Sagittarius())
     print("saved")
     emit('my_response', {'data': "traced"})
 
@@ -41,7 +41,7 @@ def readb64(b64_str):
     pimg = Image.open(sbuf)
     cv2img = np.asarray(pimg)
     #RGB->BGR
-    #return cv2img[:,:,::-1].copy()
+    return cv2img[:,:,::-1].copy()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
