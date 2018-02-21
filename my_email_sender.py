b0f133081595d6a8494879f09917d6ed45bb0322
takasa5 <vipqiv00@gmail.com>
2018-02-21 11:29:01 +0900

    歴史改変

---
+++ my_email_sender.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+
+ACCOUNT = "michibikireport@gmail.com"
+ALIAS = "michibikireport+alias@gmail.com"
+PASSWORD = os.environ.get("GOOGLE_PASS", None)
+
+def create_message(subject, content, file):
+    """file = {"name", "file"}"""
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = ACCOUNT
+    msg["To"] = ALIAS
+    msg["Date"] = formatdate(localtime=True)
+    
+    body = MIMEText(content)
+    msg.attach(body)
+    
+    #画像の場合
+    if file["file"] is not None:
+        mj = MIMEImage(file["file"], 'jpeg', filename=file["name"])
+        mj.add_header("Content-Disposition", "attachment", filename=file["name"])
+        msg.attach(mj)
+
+    return msg
+
+def send_message(message):
+
+    s = smtplib.SMTP_SSL("smtp.gmail.com")
+    s.login(ACCOUNT, PASSWORD)
+
+    s.send_message(message)
+    s.quit()
+
+    print("finished: {}".format(sys._getframe().f_code.co_name))
+
+if __name__ == "__main__":
+    pass
